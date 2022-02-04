8e7f7bf8a8281c0ed69069ff59c611ba1f266e67
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-03 16:43:39 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import './App.css';
+import MovieForm from './MovieForm';
+import MovieList from './MovieList';
+import Movie from './Movie';
 
 function App() {
   const [allMovies, setAllMovies] = useState([]);
@@ -8,7 +11,7 @@ function App() {
   const [movieFormDirector, setMovieFormDirector] = useState('');
   const [movieTitle, setMovieTitle] = useState('');
   const [movieFormColor, setMovieFormColor] = useState();
-  
+
   return (
     <div className="App">
    
