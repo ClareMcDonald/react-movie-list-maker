7a62e1d80d83d2af480524a67e4eaad6bcbb0b22
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-03 17:05:50 -0800

    passed props to Movie and Movie Form component, wrote submitMovie function

--- src/App.js
+++ src/App.js
@@ -4,17 +4,51 @@ import MovieForm from './MovieForm';
 import MovieList from './MovieList';
 import Movie from './Movie';
 
+
 function App() {
   const [allMovies, setAllMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
-  const [movieFormYearReleased, setMovieFormYearReleased] = useState(1990);
+  const [movieFormYearReleased, setMovieFormYearReleased] = useState();
   const [movieFormDirector, setMovieFormDirector] = useState('');
   const [movieTitle, setMovieTitle] = useState('');
   const [movieFormColor, setMovieFormColor] = useState();
 
+  function submitMovie(e) {
+    e.preventDefault();
+    
+    const newMovie = {
+      title: movieTitle,
+      director: movieFormDirector,
+      year: movieFormYearReleased,
+      color: movieFormColor
+    };
+  
+    setAllMovies([...allMovies, newMovie]);
+  
+    setMovieTitle('');
+    setMovieFormDirector('');
+    setMovieFormYearReleased();
+    setMovieFormColor();
+  }
+
   return (
     <div className="App">
-   
+      <Movie movie={{
+        title: movieTitle,
+        director: movieFormDirector,
+        year: movieFormYearReleased,
+        color: movieFormColor
+      }} />
+      <MovieForm
+        submitMovie={submitMovie}
+        movieTitle={movieTitle}
+        setMovieTitle={setMovieTitle}
+        movieFormYearReleased={movieFormYearReleased}
+        setMovieFormYearReleased={setMovieFormYearReleased}
+        movieFormDirector={movieFormDirector}
+        setMovieFormDirector={setMovieFormDirector}
+        movieFormColor={movieFormColor} />
+        setMovieFormColor={setMovieFormColor}
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function Movie() {
-  return <div></div>;
+export default function Movie({ movie }) {
+  return <div>
+    <div>{movie.title}</div>
+    <p>{movie.year}</p>
+    <p>{movie.director}</p>
+  </div>;
 }
