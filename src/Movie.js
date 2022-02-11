2a44f7437981699a7277f609ea1fef98de15b421
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-10 16:04:48 -0800

    refactored Movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,15 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import { Component } from 'react';
 
-export default function Movie({ movie, handleDeleteMovie }) {
-  return <div style={{ backgroundColor: movie.color }} onClick={() => handleDeleteMovie && handleDeleteMovie(movie.id)}>
-    <div>{movie.title}</div>
-    <p>{movie.year}</p>
-    <p>{movie.director}</p>
-  </div>;
+export default class Movie extends Component {
+  render() {
+    const { movie, handleDeleteMovie } = this.props;
+  
+    return <div style={{ backgroundColor: movie.color }} onClick={() => handleDeleteMovie && handleDeleteMovie(movie.id)}>
+      <div>{movie.title}</div>
+      <p>{movie.year}</p>
+      <p>{movie.director}</p>
+    </div>;
+  }
 }
